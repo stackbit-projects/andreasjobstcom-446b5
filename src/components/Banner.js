52623822f57a4687b728d016b7cc29fdd637c150
Andreas Jobst <lautundklar@gmail.com>
2021-01-05 12:37:04 +0000

    Banner.js: updated Paragraph 1 in Paragraph

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,7 +7,7 @@ const Banner = (props) => (
                 <h1>Hallo, mein Name ist Andreas Jobst</h1>
             </header>
             <div className="content">
-                <p>A responsive site template designed by HTML5 UP<br />
+                <p>Webdesign, E-Commerce, Online-Marekting<br />
                 and released under the Creative Commons.</p>
                 <ul className="actions">
                     <li><a href="#one" className="button next scrolly">Get Started</a></li>
