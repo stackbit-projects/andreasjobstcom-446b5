54682467c50f02c345903a344ac14ae4fe419af4
Andreas Jobst <lautundklar@gmail.com>
2021-01-05 12:27:18 +0000

    Banner.js: updated Header

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,7 +4,7 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1>Hi, my name is Forty</h1>
+                <h1>Wbdesign,</h1>
             </header>
             <div className="content">
                 <p>A responsive site template designed by HTML5 UP<br />
